6e878ea996e3dec85e5e1b652dafd6fbb498f12d
upccup <yaoyunsmile@163.com>
2015-12-09 21:48:29 +0800

    add countdown API

--- queue/src/main.go
+++ queue/src/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/Dataman-Cloud/seckilling/queue/src/kafka"
+	"github.com/Dataman-Cloud/seckilling/queue/src/model"
 	"github.com/labstack/echo"
 	mw "github.com/labstack/echo/middleware"
 	"github.com/spf13/viper"
@@ -12,6 +15,23 @@ import (
 // Handler
 func hello(c *echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!\n")
+}
+
+func countdown(c *echo.Context) error {
+	seckillTime := viper.GetTime("seckillTime")
+	log.Println(seckillTime)
+	curTime := time.Now().UTC()
+	data := model.CountdownData{
+		CurTime:  curTime.Unix(),
+		UnlockOn: seckillTime.Unix(),
+		Locked:   curTime.Before(seckillTime),
+	}
+
+	return c.JSON(http.StatusOK, model.CommonResponse{
+		Code:  0,
+		Data:  data,
+		Error: "",
+	})
 }
 
 func main() {
@@ -24,7 +44,9 @@ func main() {
 	e.Use(mw.Recover())
 	go kafka.StartKafkaProducer()
 	// Routes
-	e.Get("/test", hello)
+	e.Get("/hello", hello)
+
+	e.Get("/v1/events/:id", countdown)
 
 	// Start server
 	e.Run(viper.GetString("port"))
--- queue/src/model/api.go
+++ queue/src/model/api.go
@@ -1,1 +1,13 @@
 package model
+
+type CommonResponse struct {
+	Code  int
+	Data  interface{}
+	Error string
+}
+
+type CountdownData struct {
+	CurTime  int64
+	UnlockOn int64
+	Locked   bool
+}
