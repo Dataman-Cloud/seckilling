e73880bd6ef6017655745cd33d56c3089139570c
Li Guoqiang <gqli@dataman-inc.com>
2015-12-10 19:22:11 +0800

    config dir refactoring

--- queue/README.md
+++ queue/README.md
@@ -2,7 +2,7 @@
 
  * 1. git clone git@github.com:Dataman-Cloud/seckilling.git 
  * 2. cd queue
- * 3. cp ./src/queue.sample.json queue.json
+ * 3. cp ./src/queue-conf.sample.json ~/.seckilling/queue-conf.json
  * 4. make run
  
 #### 配置文件参考 [viper](https://github.com/spf13/viper)</br>
--- queue/src/configuration.go
+++ queue/src/configuration.go
@@ -11,13 +11,15 @@ func initConfig() {
 	viper.SetDefault("port", ":5090")
 	viper.SetDefault("logLevel", "DEBUG")
 
-	viper.SetConfigName("queue")
+	viper.SetConfigName("queue-conf")
 	viper.SetConfigType("json")
 	viper.AddConfigPath(".") // optionally look for config in the working directory
-	viper.AddConfigPath("$HOME/.queue")
-	viper.AddConfigPath("/etc/queue/")
+	viper.AddConfigPath("$HOME/.seckilling/")
+	viper.AddConfigPath("/etc/seckilling/")
+
 	err := viper.ReadInConfig() // Find and read the config file
 	if err != nil {             // Handle errors reading the config file
 		log.Panicf("Fatal error config file: %s \n", err)
 	}
+	log.Printf("loading config %s \n", viper.ConfigFileUsed())
 }
