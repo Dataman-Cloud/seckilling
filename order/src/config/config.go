9a382755595143c86cbab59afd549402a3ffd952
upccup <yaoyunsmile@163.com>
2015-12-16 17:47:56 +0800

    add db upgrade code

--- order/src/config/config.go
+++ order/src/config/config.go
@@ -6,13 +6,23 @@ import (
 	"github.com/spf13/viper"
 )
 
+type DbConfig struct {
+	User         string
+	Password     string
+	Host         string
+	Port         int
+	Name         string
+	MaxIdleConns int
+	MaxOpenConns int
+}
+
 //InitConfig initialize configuration
 func InitConfig() {
 	viper.SetDefault("logLevel", "DEBUG")
 	viper.SetDefault("workers", 10)
 
 	viper.SetConfigName("order-conf")
-	viper.SetConfigType("json")
+	viper.SetConfigType("yaml")
 	viper.AddConfigPath(".") // optionally look for config in the working directory
 	viper.AddConfigPath("$HOME/.seckilling/")
 	viper.AddConfigPath("/etc/seckilling/")
--- order/src/main.go
+++ order/src/main.go
@@ -1,12 +1,41 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/Dataman-Cloud/seckilling/order/src/config"
 	"github.com/Dataman-Cloud/seckilling/order/src/queue"
+	migrate "github.com/mattes/migrate/migrate"
+	"github.com/spf13/viper"
 )
 
 func main() {
 	config.InitConfig()
+	upgradeDB()
 	queue.InitClient()
 	queue.Start()
+}
+
+func upgradeDB() {
+	user := viper.GetString("db.user")
+	password := viper.GetString("db.password")
+	host := viper.GetString("db.host")
+	port := viper.GetInt("db.port")
+	name := viper.GetString("db.name")
+	driver := fmt.Sprintf("mysql://%s:%s@tcp(%s:%d)/%s", user, password, host, port, name)
+
+	log.Panicln("upgrading DB", driver)
+	errors, ok := migrate.UpSync(driver, "./sql")
+	if errors != nil && len(errors) > 0 {
+		for _, err := range errors {
+			log.Panicln("db err", err)
+		}
+		log.Panicln("can't upgrade db", errors)
+	}
+	if !ok {
+		log.Panicln("can't upgrade db")
+	}
+	log.Panicln("DB upgraded")
+
 }
