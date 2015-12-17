ea2a345e3ae0c534e2db39f5d22f8e76326db72f
upccup <yaoyunsmile@163.com>
2015-12-17 13:31:19 +0800

    uodate db oeder

--- order/sql/0001_order_init.up.sql
+++ order/sql/0001_order_init.up.sql
@@ -8,10 +8,9 @@ create table if not exists order (
     id bigint unsigned primary key auto_increment,
     uid varchar(64) not null,
     status tinyint(1) not null,
-    stock bigint(64),
-    json text,
+    sku bigint(64),
     created timestamp,
     updated timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
     ) character set utf8 collate utf8_general_ci;
 
-ALTER TABLE ORDER ADD INDEX UID ; 
+ALTER TABLE ORDER ADD INDEX UID (UID); 
