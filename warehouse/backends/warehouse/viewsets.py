14207ce6f9c08aa46695ecccb8005ccf6d5c3edd
Zhongqiang Dou <douzl0814@163.com>
2016-01-05 17:42:41 +0800

    Add the function update_activity_status
    Signed-off-by: Zhongqiang Dou <douzl0814@163.com>

--- warehouse/backends/warehouse/viewsets.py
+++ warehouse/backends/warehouse/viewsets.py
@@ -56,9 +56,24 @@ class ActivitiesViewSet(viewsets.ReadOnlyModelViewSet):
     @list_route(methods=['get'], url_path='current-activity')
     def current_activity(self, request, **kwages):
         event_key = settings.REDIS['key_fmts']['current_eid']
-        current_eid = redis_inst.get(event_key)
-        return Response({'current_eid': int(current_eid)})
-
+        current_eid = int(redis_inst.get(event_key))
+        event_key = settings.REDIS['key_fmts']['event_hash'] % str(int(current_eid))
+        status = int(redis_inst.hget(event_key, 'status'))
+        update_activity_status(current_eid, status)
+        return Response({'current_eid': current_eid})
 
     queryset = Activities.objects.all()
     serializer_class = ActivitiesSerializer
+
+
+def update_activity_status(eid, status, **kwargs):
+    event_status_mapping = {
+        1: 'running',
+        2: 'waiting',
+        3: 'end'
+    }
+    qs = Activities.objects.get(id=eid)
+    status = event_status_mapping[status]
+    if qs.status != status:
+        qs.status=status
+        qs.save()
