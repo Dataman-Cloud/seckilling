1565c0ecaba4088717e8c5a5895ace2465dbd245
Li Guoqiang <gqli@dataman-inc.com>
2016-01-11 23:27:47 +0800

    fixed token duplicated

--- proxy/lua/handle_seckill.lua
+++ proxy/lua/handle_seckill.lua
@@ -1,3 +1,4 @@
+local args = ngx.req.get_uri_args()
 local config = require "config"
 local ck = require "resty.cookie"
 local cookie, err = ck:new()
@@ -6,12 +7,15 @@ if not cookie then
     ngx.exit(ngx.HTTP_NOT_ACCEPTABLE)
     return
 end
-local token = cookie:get(config.tokenCookie)
 
 local redisc = require "redisc"
 local redis = redisc:new()
-local res, err = redis:hget("tk:"..token, "status")
+local token = args[config.tokenCookie]
+local cjson = require "cjson"
+print("get args ", cjson.encode(args), " get token ", token)
+local res, err = redis:hget("tk:"..token..args.id , "status")
 
 local uuid = require "uuid4"
 local coupon = uuid.getUUID()
 ngx.say(string.format('{"coupon":"%s"}', coupon))
+
