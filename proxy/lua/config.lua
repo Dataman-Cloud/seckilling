f45374fb1acd1924dc7194c9d5d9c897011f209b
upccup <yaoyunsmile@163.com>
2016-01-11 21:39:32 +0800

    add redis config to env

--- proxy/lua/config.lua
+++ proxy/lua/config.lua
@@ -3,9 +3,9 @@ local _M = {}
 -- redis connection
 local redis = {} 
 -- redis.host = "123.59.58.58"
-redis.host = "123.59.58.58"
+redis.host = os.getenv("REDIS_HOST") or "123.59.58.58"
 --redis.port = 5506
-redis.port = 5506
+redis.port =  os.getenv("REDIS_PORT") or 5506
 --redis.password = "UQPqcj7nUyii38cpYcr9OnTbIJ3dHXvJ"
 _M.redis = redis
 
